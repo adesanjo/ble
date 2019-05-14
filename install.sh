645ae74b2918cb7f9517f033b7f8d044098716f3
Adrian Sanjo <jesuisanonime@gmail.com>
2019-05-14 18:19:26 +0200

    y

--- install.sh
+++ install.sh
@@ -2,13 +2,17 @@
 
 mkdir -p $HOME/ble
 curl https://codeload.github.com/adesanjo/ble/zip/master > $HOME/ble/master.zip
+
+echo "Unzipping..."
 unzip -o -qq $HOME/ble/master.zip -d $HOME/ble
 rm $HOME/ble/master.zip
-if grep -q "export PATH=$PATH:$HOME/ble/ble-master" $HOME/.bashrc
+
+echo "Finalising..."
+if ! grep -q "export PATH=$PATH:$HOME/ble/ble-master" $HOME/.bashrc
 then
     echo "export PATH=$PATH:$HOME/ble/ble-master" >> $HOME/.bashrc
 fi
-if grep -q "export PATH=$PATH:$HOME/ble/ble-master" $HOME/.zshrc
+if ! grep -q "export PATH=$PATH:$HOME/ble/ble-master" $HOME/.zshrc
 then
     echo "export PATH=$PATH:$HOME/ble/ble-master" >> $HOME/.zshrc
 fi
