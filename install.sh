adbfb4a496f7ff549e47270b8a977b1497c89705
Adrian Sanjo <jesuisanonime@gmail.com>
2019-05-14 17:59:43 +0200

    y

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 mkdir -p $HOME/ble
 curl https://codeload.github.com/adesanjo/ble/zip/master > $HOME/ble/master.zip
 cd $HOME/ble
-unzip -o master.zip
+unzip -o -qq master.zip
 rm master.zip
 cd $HOME
 echo "export PATH=$PATH:$HOME/ble/ble-master" >> $HOME/.bashrc
