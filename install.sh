266f91d1a47462a480d2acbf1578cd271e06da88
Adrian Sanjo <jesuisanonime@gmail.com>
2019-05-14 16:59:07 +0200

    y

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 mkdir -p $HOME/ble
-wget https://github.com/adesanjo/ble/archive/master.zip > $HOME/ble/master.zip
+curl https://github.com/adesanjo/ble/archive/master.zip > $HOME/ble/master.zip
 cd $HOME/ble
 unzip master.zip
