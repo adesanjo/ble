d5940569fd13d349e0977d1547e337792b5b346a
Adrian Sanjo <jesuisanonime@gmail.com>
2019-06-09 09:55:46 +0200

    y

--- languageLexer.py
+++ languageLexer.py
@@ -109,7 +109,7 @@ class Lexer:
         while self.char is not None:
             if self.char in " \t\r\n":
                 self.advance()
-            elif self.char in DIGITS:
+            elif self.char in DIGITS + ".":
                 tokens.append(self.makeNumber())
             elif self.char in LETTERS:
                 tokens.append(self.makeIdentifier())
@@ -190,7 +190,8 @@ class Lexer:
                 if hasDot:
                     break
                 hasDot = True
-            numStr += self.char
+            if self.char != "_":
+                numStr += self.char
             self.advance()
 
         if hasDot:
